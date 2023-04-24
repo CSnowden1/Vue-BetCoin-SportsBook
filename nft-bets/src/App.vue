4ce4454d47671b1a4f90bf8fe50b346cad985804
CSnowden1 <christopher.snowden@protonmail.com>
2023-04-24 11:21:13 -0400

    added body folder and move home components there

--- nft-bets/src/App.vue
+++ nft-bets/src/App.vue
@@ -1,22 +1,10 @@
 <script setup>
-import Nav from './components/Nav.vue'
-import SubNav from './components/SubNav.vue'
-import Promotron from './components/Promotron.vue'
-import Ticker from './components/Ticker.vue'
-import LeftPanel from './components/LeftPanel.vue'
-import BettingCardGrid from './components/BettingCardGrid.vue'
-import AuctionPanel from './components/AuctionPanel.vue'
+import HomeBody from './components/body/homeBody.vue';
 
 </script>
 
 <template>
-      <Nav />
-      <SubNav />
-      <Promotron />
-      <Ticker />
-      <LeftPanel />
-      <BettingCardGrid />
-      <AuctionPanel />
+      <HomeBody />
 </template>
 
 <style scoped>
---
+++ nft-bets/src/components/body/homeBody.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Nav from '../Nav.vue'
+import SubNav from '../SubNav.vue'
+import Promotron from '../Promotron.vue'
+import Ticker from '../Ticker.vue'
+import LeftPanel from '../LeftPanel.vue'
+import BettingCardGrid from '../BettingCardGrid.vue'
+import AuctionPanel from '../AuctionPanel.vue'
+
+</script>
+
+<template>
+      <Nav />
+      <SubNav />
+      <Promotron />
+      <Ticker />
+      <LeftPanel />
+      <BettingCardGrid />
+      <AuctionPanel />
+</template>
+
+<style scoped>
+/* add any necessary styling */
+</style>
